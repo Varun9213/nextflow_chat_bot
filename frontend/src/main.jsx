4d7d5ad8f7e6ec28308ea989ed670341f46c4d6c
Varun Dutt <94442581+dutvar@users.noreply.github.com>
2025-09-21 22:25:38 +0100

    comments cleaning

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./App.css";   // <--- use app.css for layout
+import "./App.css"; 
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
